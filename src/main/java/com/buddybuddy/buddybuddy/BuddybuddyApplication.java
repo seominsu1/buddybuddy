fd5d7ebc40308ff1ebc5b5e1f22ceedaf644857b
usnim <sms897@naver.com>
2023-12-20 17:30:14 +0900

    최초세팅

---
+++ src/main/java/com/buddybuddy/buddybuddy/BuddybuddyApplication.java
@@ -0,0 +1,13 @@
+package com.buddybuddy.buddybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddybuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddybuddyApplication.class, args);
+	}
+
+}
